14d0abe64863bcc78228b3f88249946e7542bf2e
phung <maiphung2017@gmail.com>
2021-03-19 01:27:10 +0700

    Build Rest API

---
+++ SpyStore.Service/Controllers/SearchController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpyStore.Dal.Repos.Interfaces;
+using SpyStore.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpyStore.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+
+        private readonly IProductRepo _repo;
+        
+        public SearchController(IProductRepo repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("{searchString}", Name = "SearchProducts")]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(500)]
+        public ActionResult<IList<Product>> Search(string searchString)
+        {
+            var results = _repo.Search(searchString).ToList();
+            if (results.Count == 0)
+            {
+                return NoContent();
+            }
+            return results;
+        }
+
+    }
+}
--- SpyStore.Service/Startup.cs
+++ SpyStore.Service/Startup.cs
@@ -2,29 +2,92 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using SpyStore.Dal.EfStructures;
+using SpyStore.Dal.Initialization;
+using SpyStore.Dal.Repos;
+using SpyStore.Dal.Repos.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
+using SpyStore.Service.Filters;
 
 namespace SpyStore.Service
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
+            _env = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        private readonly IHostingEnvironment _env;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore(config => config.Filters.Add(new SpyStoreExceptionFilter(_env)))
+                .AddJsonFormatters(j =>
+                {
+                    j.ContractResolver = new DefaultContractResolver();
+                    j.Formatting = Formatting.Indented;
+                });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin()
+                    .AllowCredentials();
+                });
+            });
+
+            services.AddDbContextPool<StoreContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SpyStore")));
+
+            services.AddScoped<ICategoryRepo, CategoryRepo>();
+            services.AddScoped<IProductRepo, ProductRepo>();
+            services.AddScoped<ICustomerRepo, CustomerRepo>();
+            services.AddScoped<IShoppingCartRepo, ShoppingCartRepo>();
+            services.AddScoped<IOrderRepo, OrderRepo>();
+            services.AddScoped<IOrderDetailRepo, OrderDetailRepo>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "SpyStore Service",
+                        Version = "v1",
+                        Description = "Service to support the SpyStore sample eCommerce site",
+                        //TermsOfService = new Uri("None"),
+                        License = new OpenApiLicense
+                        {
+                            Name = "Freeware",
+                            Url = new Uri("http://localhost:44335/LICENSE.txt")
+                        }
+                    });
+                    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                    c.IncludeXmlComments(xmlPath);
+                });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -34,13 +97,27 @@ namespace SpyStore.Service
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    var context =
+                    serviceScope.ServiceProvider.GetRequiredService<StoreContext>();
+                    SampleDataInitializer.InitializeData(context);
+                }
             }
             else
             {
                 app.UseHsts();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SpyStore Service v1");
+            });
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
+            app.UseCors("AllowAll");
             app.UseMvc();
         }
     }
